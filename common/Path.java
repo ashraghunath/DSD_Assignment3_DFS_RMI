c45625958243b90b4cbc9be7cf9e8ff281ec1012
Ashwin Raghunath <ashwinr2296@gmail.com>
2021-10-30 10:02:23 -0400

    HA from here

--- common/Path.java
+++ common/Path.java
@@ -21,257 +21,333 @@ import java.util.*;
  */
 public class Path implements Iterable<String>, Serializable
 {
-    private List<String> path;
+    private String filePath;
+    private List<String> componentList;
+
+
+    private void setValues(String path)
+    {
+        if (path == null)
+        {
+            throw new IllegalArgumentException("Path cannot be null string");
+        }
+
+        if (path.length() == 0)
+        {
+            throw new IllegalArgumentException("Path cannot be empty string");
+        }
+
+        if (!path.startsWith("/"))
+        {
+            throw new IllegalArgumentException("Path should start with root directory");
+        }
+
+        if (path.contains(":"))
+        {
+            throw new IllegalArgumentException("Path cannot contain invalid characters");
+        }
+
+        StringBuffer sBuffer = new StringBuffer("/");
+        componentList = new ArrayList<>();
+
+        String[] components  = path.split("/");
+
+        for (String c : components)
+        {
+            if (c.length() == 0)
+            {
+                continue;
+            }
+            componentList.add(c);
+            if (sBuffer.charAt(sBuffer.length() - 1) != '/')
+            {
+                sBuffer.append('/');
+            }
+            sBuffer.append(c);
+        }
+
+        filePath = sBuffer.toString();
+
+    }
+
+
     /** Creates a new path which represents the root directory. */
     public Path()
     {
-        this.path = new ArrayList<String>();
+        String rootPath = "/";
+        setValues(rootPath);
     }
 
     /** Creates a new path by appending the given component to an existing path.
 
-        @param path The existing path.
-        @param component The new component.
-        @throws IllegalArgumentException If <code>component</code> includes the
-                                         separator, a colon, or
-                                         <code>component</code> is the empty
-                                         string.
-    */
+     @param path The existing path.
+     @param component The new component.
+     @throws IllegalArgumentException If <code>component</code> includes the
+     separator, a colon, or
+     <code>component</code> is the empty
+     string.
+     */
     public Path(Path path, String component)
     {
-        if(!Objects.nonNull(component) || component.isEmpty() || !isValidComponent(component))
-            throw new IllegalArgumentException("invalid component string");
-        Iterator<String> pathIt = path.iterator();
-        this.path = new ArrayList<String>();
-        while (pathIt.hasNext()) {
-            this.path.add(pathIt.next());
+        if (component == null)
+        {
+            throw new IllegalArgumentException("Path component cannot be null string");
         }
-        this.path.add(component);
 
-    }
-
-    //Checks if component has separators
-    private boolean isValidComponent(String component) {
-        for (char c : component.toCharArray()) {
-            if(c=='/' || c==':')
-                return false;
+        if (component.length() == 0)
+        {
+            throw new IllegalArgumentException("Path component cannot be empty string");
         }
-        return true;
+
+        if (component.contains("/") || component.contains(":"))
+        {
+            throw new IllegalArgumentException("Path component cannot contain invalid characters");
+        }
+
+        String newPath = path.filePath + "/" + component;
+        setValues(newPath);
     }
 
     /** Creates a new path from a path string.
 
-        <p>
-        The string is a sequence of components delimited with forward slashes.
-        Empty components are dropped. The string must begin with a forward
-        slash.
+     <p>
+     The string is a sequence of components delimited with forward slashes.
+     Empty components are dropped. The string must begin with a forward
+     slash.
 
-        @param path The path string.
-        @throws IllegalArgumentException If the path string does not begin with
-                                         a forward slash, or if the path
-                                         contains a colon character.
+     @param path The path string.
+     @throws IllegalArgumentException If the path string does not begin with
+     a forward slash, or if the path
+     contains a colon character.
      */
     public Path(String path)
     {
+        setValues(path);
+    }
 
-        if (path == null)
-            throw new IllegalArgumentException("null path found");
-        if (path.length() == 0 || path.charAt(0) != '/')
-            throw new IllegalArgumentException("no / in beginning of path");
-        this.path = new ArrayList<String>();
-        for (String s : path.split("/")) {
-            if (!isValidComponent(s)) {
-                throw new IllegalArgumentException("path contains / or :");
-            } else if (s.length() == 0) {
-                continue;
-            } else {
-                this.path.add(s);
-            }
+
+    private class PathIterator implements Iterator<String>
+    {
+        private Iterator<String> iter;
+
+        public PathIterator(Iterator<String> iter)
+        {
+            this.iter = iter;
+        }
+
+        @Override
+        public boolean hasNext()
+        {
+            return iter.hasNext();
+        }
+
+        @Override
+        public String next()
+        {
+            return iter.next();
+        }
+
+        @Override
+        public void remove()
+        {
+            throw new UnsupportedOperationException();
         }
     }
 
+
     /** Returns an iterator over the components of the path.
 
-        <p>
-        The iterator cannot be used to modify the path object - the
-        <code>remove</code> method is not supported.
+     <p>
+     The iterator cannot be used to modify the path object - the
+     <code>remove</code> method is not supported.
 
-        @return The iterator.
+     @return The iterator.
      */
     @Override
     public Iterator<String> iterator()
     {
-        return new Iterator<String>() {
-            private int i = 0;
-
-            public boolean hasNext() {
-                if (i >= path.size())
-                    return false;
-                return true;
-            }
-
-            public String next() {
-                String temp = "";
-                if (i < path.size())
-                    temp = path.get(i);
-                else {
-                    throw new NoSuchElementException(
-                            "Trying to advance past end of iterator.");
-                }
-                i++;
-                return temp;
-            }
-
-        };
+        return new PathIterator(componentList.iterator());
     }
 
     /** Lists the paths of all files in a directory tree on the local
-        filesystem.
+     filesystem.
 
-        @param directory The root directory of the directory tree.
-        @return An array of relative paths, one for each file in the directory
-                tree.
-        @throws FileNotFoundException If the root directory does not exist.
-        @throws IllegalArgumentException If <code>directory</code> exists but
-                                         does not refer to a directory.
+     @param directory The root directory of the directory tree.
+     @return An array of relative paths, one for each file in the directory
+     tree.
+     @throws FileNotFoundException If the root directory does not exist.
+     @throws IllegalArgumentException If <code>directory</code> exists but
+     does not refer to a directory.
      */
     public static Path[] list(File directory) throws FileNotFoundException
     {
+        if (directory == null)
+        {
+            throw new FileNotFoundException("Null directory not found");
+        }
 
-        ArrayList<Path> paths = findFilePathFromDirectory(directory, directory.getPath()
-                .length());
-        return paths.toArray(new Path[paths.size()]);
+        if (!directory.exists())
+        {
+            throw new FileNotFoundException(directory.getPath() + "does not exist");
+        }
+
+        if (!directory.isDirectory())
+        {
+            throw new IllegalArgumentException(directory.getPath() +  "is not a directory");
+        }
+
+        List<String> files = getAllFiles(directory, "");
+        Path[]       paths = new Path[files.size()];
+
+
+        int startInd = directory.getName().length() + 1;
+
+        for (int i =0 ; i < files.size() ; i++)
+        {
+            paths[i] = new Path(files.get(i).substring(startInd));
+        }
+
+        return paths;
+    }
+
+    private static List<String> getAllFiles(File file, String path)
+    {
+        List<String> files = new ArrayList<>();
+
+        if (file.isDirectory())
+        {
+            String exPath = path + "/" + file.getName();
+            for (File f : file.listFiles())
+            {
+                files.addAll(getAllFiles(f, exPath));
+            }
+        }
+        else
+        {
+
+            files.add(path + "/" + file.getName());
+        }
+
+        return files;
     }
 
     /** Determines whether the path represents the root directory.
 
-        @return <code>true</code> if the path does represent the root directory,
-                and <code>false</code> if it does not.
+     @return <code>true</code> if the path does represent the root directory,
+     and <code>false</code> if it does not.
      */
     public boolean isRoot()
     {
-        return this.path.isEmpty();
+        return (filePath.equals("/"));
     }
 
     /** Returns the path to the parent of this path.
 
-        @throws IllegalArgumentException If the path represents the root
-                                         directory, and therefore has no parent.
+     @throws IllegalArgumentException If the path represents the root
+     directory, and therefore has no parent.
      */
     public Path parent()
     {
-        if(isRoot())
+        if (isRoot())
         {
-            throw new IllegalArgumentException("Root has no parent ");
+            throw new IllegalArgumentException("Cannot find parent of root directory");
         }
-        String parentPath = "";
-        for(int index=0; index<this.path.size()-1; index++)
+
+        StringBuffer sBuffer = new StringBuffer("/");
+
+        for (int i = 0 ; i < componentList.size() - 1; i++)
         {
-            parentPath+="/"+this.path.get(0);
+            sBuffer.append("/").append(componentList.get(i));
         }
-        return new Path(parentPath);
+
+        return new Path(sBuffer.toString());
     }
 
     /** Returns the last component in the path.
 
-        @throws IllegalArgumentException If the path represents the root
-                                         directory, and therefore has no last
-                                         component.
+     @throws IllegalArgumentException If the path represents the root
+     directory, and therefore has no last
+     component.
      */
     public String last()
     {
-        if (this.isRoot())
-            throw new IllegalArgumentException(
-                    "Current path is the root.(Thrown from last())");
-        return this.path.get(this.path.size() - 1);
+        if (isRoot())
+        {
+            throw new IllegalArgumentException("Cannot find parent of root directory");
+        }
+
+        return componentList.get(componentList.size() - 1);
     }
 
     /** Determines if the given path is a subpath of this path.
 
-        <p>
-        The other path is a subpath of this path if is a prefix of this path.
-        Note that by this definition, each path is a subpath of itself.
+     <p>
+     The other path is a subpath of this path if is a prefix of this path.
+     Note that by this definition, each path is a subpath of itself.
 
-        @param other The path to be tested.
-        @return <code>true</code> If and only if the other path is a subpath of
-                this path.
+     @param other The path to be tested.
+     @return <code>true</code> If and only if the other path is a subpath of
+     this path.
      */
     public boolean isSubpath(Path other)
     {
-        String thisPath = this.toString();
-        String otherPath = other.toString();
-        return thisPath.startsWith(otherPath);
+        return (filePath.contains(other.filePath));
     }
 
     /** Converts the path to <code>File</code> object.
 
-        @param root The resulting <code>File</code> object is created relative
-                    to this directory.
-        @return The <code>File</code> object.
+     @param root The resulting <code>File</code> object is created relative
+     to this directory.
+     @return The <code>File</code> object.
      */
     public File toFile(File root)
     {
-        //not called anywhere
-        return new File(root, this.toString());
+        if (root != null)
+        {
+            return new File(root, this.toString());
+        }
+        else {
+            return new File(this.toString());
+        }
     }
 
     /** Compares two paths for equality.
 
-        <p>
-        Two paths are equal if they share all the same components.
+     <p>
+     Two paths are equal if they share all the same components.
 
-        @param other The other path.
-        @return <code>true</code> if and only if the two paths are equal.
+     @param other The other path.
+     @return <code>true</code> if and only if the two paths are equal.
      */
     @Override
     public boolean equals(Object other)
     {
-        return this.toString().equals(other.toString());
+        if (!(other instanceof Path))
+        {
+            return false;
+        }
+
+        return filePath.equals(((Path)other).filePath);
     }
 
     /** Returns the hash code of the path. */
     @Override
     public int hashCode()
     {
-        return this.toString().hashCode();
+        return filePath.hashCode();
     }
 
     /** Converts the path to a string.
 
-        <p>
-        The string may later be used as an argument to the
-        <code>Path(String)</code> constructor.
+     <p>
+     The string may later be used as an argument to the
+     <code>Path(String)</code> constructor.
 
-        @return The string representation of the path.
+     @return The string representation of the path.
      */
     @Override
     public String toString()
     {
-        if (this.path.size() == 0)
-            return "/";
-
-        String path = "";
-        for (int i = 0; i < this.path.size(); i++) {
-            path += "/" + this.path.get(i);
-        }
-        return path;
+        return filePath;
     }
-
-    private static ArrayList<Path> findFilePathFromDirectory(File directory, int parentLength) {
-        ArrayList<Path> pathsFound = new ArrayList<Path>();
-
-        for (File fileMaybe : directory.listFiles()) {
-            if (fileMaybe.isFile()) {
-                pathsFound.add(new Path(fileMaybe.getPath().substring(parentLength)));
-            } else if (fileMaybe.isDirectory()) {
-                for (Path p : findFilePathFromDirectory(fileMaybe, parentLength))
-                    pathsFound.add(p);
-            }
-        }
-
-        //Todo not sure. maybe need to reverse collections later.
-        return pathsFound;
-    }
-
-
 }
--- naming/NamingServer.java
+++ naming/NamingServer.java
@@ -4,69 +4,86 @@ import java.io.*;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Random;
 
 import rmi.*;
 import common.*;
 import storage.*;
 
+
+
+
+class ServerStub {
+    public Storage storageStub;
+    public Command commandStub;
+
+    public ServerStub(Storage storage, Command command){
+        storageStub = storage;
+        commandStub = command;
+    }
+
+    public boolean equals(ServerStub serverStub){
+        return storageStub.equals(serverStub.storageStub) && commandStub.equals(serverStub.commandStub);
+    }
+
+}
+
+
 /** Naming server.
 
-    <p>
-    Each instance of the filesystem is centered on a single naming server. The
-    naming server maintains the filesystem directory tree. It does not store any
-    file data - this is done by separate storage servers. The primary purpose of
-    the naming server is to map each file name (path) to the storage server
-    which hosts the file's contents.
+ <p>
+ Each instance of the filesystem is centered on a single naming server. The
+ naming server maintains the filesystem directory tree. It does not store any
+ file data - this is done by separate storage servers. The primary purpose of
+ the naming server is to map each file name (path) to the storage server
+ which hosts the file's contents.
 
-    <p>
-    The naming server provides two interfaces, <code>Service</code> and
-    <code>Registration</code>, which are accessible through RMI. Storage servers
-    use the <code>Registration</code> interface to inform the naming server of
-    their existence. Clients use the <code>Service</code> interface to perform
-    most filesystem operations. The documentation accompanying these interfaces
-    provides details on the methods supported.
+ <p>
+ The naming server provides two interfaces, <code>Service</code> and
+ <code>Registration</code>, which are accessible through RMI. Storage servers
+ use the <code>Registration</code> interface to inform the naming server of
+ their existence. Clients use the <code>Service</code> interface to perform
+ most filesystem operations. The documentation accompanying these interfaces
+ provides details on the methods supported.
 
-    <p>
-    Stubs for accessing the naming server must typically be created by directly
-    specifying the remote network address. To make this possible, the client and
-    registration interfaces are available at well-known ports defined in
-    <code>NamingStubs</code>.
+ <p>
+ Stubs for accessing the naming server must typically be created by directly
+ specifying the remote network address. To make this possible, the client and
+ registration interfaces are available at well-known ports defined in
+ <code>NamingStubs</code>.
  */
 public class NamingServer implements Service, Registration
 {
-
+    private Skeleton<Service> serviceSkeleton = null;
+    private Skeleton<Registration> registrationSkeleton = null;
     private boolean started = false;
     private boolean stopping = false;
-    private Skeleton<Registration> registrationSkeleton = null;
-    private Skeleton<Service> serviceSkeleton = null;
-    private Random random = null;
-    private List<ServerStub> serverStubList = null;
-    private HashTree tree = null;
+    private LinkedList<ServerStub> stubList = new LinkedList<>();
+    private HashTree hashTree = new HashTree(stubList);
+
+    private static Random randGenerator = new Random();
+
+
 
     /** Creates the naming server object.
 
-        <p>
-        The naming server is not started.
+     <p>
+     The naming server is not started.
      */
     public NamingServer()
     {
-        serverStubList = new LinkedList<>();
-        random =  new Random();
-
     }
 
     /** Starts the naming server.
 
-        <p>
-        After this method is called, it is possible to access the client and
-        registration interfaces of the naming server remotely.
+     <p>
+     After this method is called, it is possible to access the client and
+     registration interfaces of the naming server remotely.
 
-        @throws RMIException If either of the two skeletons, for the client or
-                             registration server interfaces, could not be
-                             started. The user should not attempt to start the
-                             server again if an exception occurs.
+     @throws RMIException If either of the two skeletons, for the client or
+     registration server interfaces, could not be
+     started. The user should not attempt to start the
+     server again if an exception occurs.
      */
     public synchronized void start() throws RMIException
     {
@@ -92,12 +109,12 @@ public class NamingServer implements Service, Registration
 
     /** Stops the naming server.
 
-        <p>
-        This method waits for both the client and registration interface
-        skeletons to stop. It attempts to interrupt as many of the threads that
-        are executing naming server code as possible. After this method is
-        called, the naming server is no longer accessible remotely. The naming
-        server should not be restarted.
+     <p>
+     This method waits for both the client and registration interface
+     skeletons to stop. It attempts to interrupt as many of the threads that
+     are executing naming server code as possible. After this method is
+     called, the naming server is no longer accessible remotely. The naming
+     server should not be restarted.
      */
     public void stop()
     {
@@ -125,12 +142,12 @@ public class NamingServer implements Service, Registration
 
     /** Indicates that the server has completely shut down.
 
-        <p>
-        This method should be overridden for error reporting and application
-        exit purposes. The default implementation does nothing.
+     <p>
+     This method should be overridden for error reporting and application
+     exit purposes. The default implementation does nothing.
 
-        @param cause The cause for the shutdown, or <code>null</code> if the
-                     shutdown was by explicit user request.
+     @param cause The cause for the shutdown, or <code>null</code> if the
+     shutdown was by explicit user request.
      */
     protected void stopped(Throwable cause)
     {
@@ -140,31 +157,30 @@ public class NamingServer implements Service, Registration
     @Override
     public boolean isDirectory(Path path) throws FileNotFoundException
     {
-        return this.tree.isDirectory(path);
+        return hashTree.isDirectory(path);
     }
 
     @Override
     public String[] list(Path directory) throws FileNotFoundException
     {
-        return this.tree.list(directory);
+        return hashTree.list(directory);
     }
 
     @Override
-    public boolean createFile(Path file)
-        throws RMIException, FileNotFoundException
+    public boolean createFile(Path file) throws RMIException, FileNotFoundException
     {
         if(file.isRoot())
             return false;
 
-        ServerStub serverStub = serverStubList.get(random.nextInt(serverStubList.size()));
+        ServerStub serverStub = stubList.get(randGenerator.nextInt(stubList.size()));
 
 
-        if(!tree.isDirectory(file.parent()))
+        if(!hashTree.isDirectory(file.parent()))
         {
             throw new FileNotFoundException("The parent of "+file.toString()+" is not a directory.");
         }
 
-        boolean success = tree.createFile(file, serverStub);
+        boolean success = hashTree.createFile(file, serverStub);
 
         if(!success)
             return false;
@@ -173,10 +189,11 @@ public class NamingServer implements Service, Registration
 
         if(!success)
         {
-            this.tree.delete(file);
+            hashTree.delete(file);
         }
 
         return success;
+
     }
 
     @Override
@@ -185,30 +202,30 @@ public class NamingServer implements Service, Registration
         if(directory.isRoot())
             return false;
 
-        if(!this.tree.isDirectory(directory.parent()))
+        if(!hashTree.isDirectory(directory.parent()))
         {
             throw new FileNotFoundException("The parent of " + directory.toString() + " is not a directory.");
         }
 
-        return this.tree.createDirectory(directory);
+        return hashTree.createDirectory(directory);
+
     }
 
     @Override
     public boolean delete(Path path) throws FileNotFoundException
     {
-        return (!path.isRoot()) && this.tree.delete(path);
+        return (!path.isRoot()) && hashTree.delete(path);
     }
 
     @Override
     public Storage getStorage(Path file) throws FileNotFoundException
     {
-        return this.tree.getStorage(file).storageStub;
+        return hashTree.getStorage(file).storageStub;
     }
 
     // The method register is documented in Registration.java.
     @Override
-    public Path[] register(Storage client_stub, Command command_stub,
-                           Path[] files)
+    public Path[] register(Storage client_stub, Command command_stub, Path[] files)
     {
         if (client_stub == null || command_stub == null || files == null)
         {
@@ -216,9 +233,9 @@ public class NamingServer implements Service, Registration
         }
 
         ArrayList<Path> deleteList = new ArrayList<>();
-        ServerStub newStub = new ServerStub(command_stub, client_stub);
+        ServerStub newStub = new ServerStub(client_stub, command_stub);
 
-        for (ServerStub s : serverStubList)
+        for (ServerStub s : stubList)
         {
             if (newStub.equals(s))
             {
@@ -226,14 +243,14 @@ public class NamingServer implements Service, Registration
             }
         }
 
-        synchronized (serverStubList)
+        synchronized (stubList)
         {
-            serverStubList.add(newStub);
+            stubList.add(newStub);
         }
 
         for (Path path : files)
         {
-            if (!path.isRoot() && !this.tree.createFileRecursive(path, newStub))
+            if (!path.isRoot() && !hashTree.createFileRecursive(path, newStub))
             {
                 deleteList.add(path);
             }
@@ -247,16 +264,3 @@ public class NamingServer implements Service, Registration
 }
 
 
-class ServerStub{
-    public Command commandStub;
-    public Storage storageStub;
-
-    public ServerStub(Command commandStub, Storage storageStub) {
-        this.commandStub = commandStub;
-        this.storageStub = storageStub;
-    }
-
-    public boolean equals(ServerStub serverStub) {
-        return storageStub.equals(serverStub.storageStub) && commandStub.equals(serverStub.commandStub);
-    }
-}
